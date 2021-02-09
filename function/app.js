cec3fd3381ea93b22211ea207171959c4c7cdb83
Emran Muhammad Aloul <emranaloul1992@hotmail.com>
2021-02-09 14:49:06 +0200

    making js function

---
+++ function/app.js
@@ -0,0 +1,11 @@
+alert("do you want to know your birth year??");
+
+var x = prompt('please enter you current age');
+
+ 
+function birthdayYear(y){
+    document.write('your birth year is '  + `${y = 2020 - x}`)
+}
+
+birthdayYear()
+
